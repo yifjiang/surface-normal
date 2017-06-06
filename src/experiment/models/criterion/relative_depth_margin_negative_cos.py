4e89263ca1b2472a1812fb4f5aa03a06ee42ba9c
yifjiang <james970618@sjtu.edu.cn>
2017-06-06 16:48:08 -0400

    bug fixing

--- src/experiment/models/criterion/relative_depth_margin_negative_cos.py
+++ src/experiment/models/criterion/relative_depth_margin_negative_cos.py
@@ -5,3 +5,6 @@ from torch.autograd import Variable
 from relative_depth_margin import relative_depth_crit
 from normal_negative_cos import normal_negative_cos_crit
 
+from .. import img_coord_to_world_coord
+from .. import world_coord_to_normal
+

655f3cb6cbb63c047aca843807851845ab1ba052
yifjiang <james970618@sjtu.edu.cn>
2017-06-08 16:02:09 -0400

    bug fixing

--- src/experiment/models/criterion/relative_depth_margin_negative_cos.py
+++ src/experiment/models/criterion/relative_depth_margin_negative_cos.py
@@ -2,11 +2,11 @@ import torch
 from torch import nn
 from torch.autograd import Variable
 
-from .relative_depth_margin import relative_depth_crit
-from .normal_negative_cos import normal_negative_cos_crit
+from relative_depth_margin import relative_depth_crit
+from normal_negative_cos import normal_negative_cos_crit
 
-from ..img_coord_to_world_coord import img_coord_to_world_coord
-from ..world_coord_to_normal import world_coord_to_normal
+from img_coord_to_world_coord import img_coord_to_world_coord
+from world_coord_to_normal import world_coord_to_normal
 
 class relative_depth_negative_cos(nn.Module):
 	"""docstring for relative_depth_negative_cos"""

78d11cab6393e546213207ddcb1913224bc7f0a8
yifjiang <james970618@sjtu.edu.cn>
2017-06-12 20:51:16 -0400

    load_data.py

--- src/experiment/load_data.py
+++ src/experiment/load_data.py
@@ -2,6 +2,9 @@ import sys
 train_depth_path = None
 valid_depth_path = None
 
+train_normal_path = None
+valid_normal_path = None
+
 folderpath = '../../data/'
 
 if g_args.t_depth_file != '':
@@ -9,6 +12,12 @@ if g_args.t_depth_file != '':
 
 if g_args.v_depth_file != '':
 	valid_depth_path = folderpath + g_args.v_depth_file
+
+if g_args.t_normal_file != '';
+	train_normal_path = folderpath + g_args.t_normal_file
+
+if g_args.v_normal_file != '':
+	valid_normal_path = folderpath + g_args.v_normal_file
 
 
 if train_depth_path is None:
@@ -19,8 +28,18 @@ if valid_depth_path is None:
 	print("Error: Missing validation file for depth!")
 	sys.exit(1)
 
+if train_normal_path is None and train_depth_path is None:
+	print("Error: No training files at all.")
+	sys.exit(1)
+
+if (train_normal_path is None and valid_normal_path is not None) or (train_normal_path is not None and valid_normal_path is not None):
+	print("Error: Either train_normal_path or valid_normal_path is not valid")
+	sys.exit(1)
+
 
 def TrainDataLoader():
+	_train_depth_path = train_depth_path
+	_train_normal_path = train_normal_path
 	return DataLoader(train_depth_path)
 
 def ValidDataLoader():
